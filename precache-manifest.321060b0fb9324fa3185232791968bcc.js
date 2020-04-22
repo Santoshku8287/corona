4414ea79987ac3957686565b40a2116f4d4e39b9
Santosh Kumar <santoshku8287@gmail.com>
2020-04-22 20:57:25 +0530

    Updates

---
+++ precache-manifest.321060b0fb9324fa3185232791968bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275448fb76c49b4f05b4d6ab1bf9eff7",
+    "url": "/github.io/corona/index.html"
+  },
+  {
+    "revision": "5589481744293bddd21f",
+    "url": "/github.io/corona/static/css/main.428cbc5d.chunk.css"
+  },
+  {
+    "revision": "7ab50aab1f4e9f7247e7",
+    "url": "/github.io/corona/static/js/2.a1d02ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/corona/static/js/2.a1d02ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5589481744293bddd21f",
+    "url": "/github.io/corona/static/js/main.d753a682.chunk.js"
+  },
+  {
+    "revision": "3d6bb65627f2c78e1ff2",
+    "url": "/github.io/corona/static/js/runtime-main.b1c86acc.js"
+  }
+]);
